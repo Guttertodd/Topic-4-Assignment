30f95c8f0e66ebfd33f6dd12d593a82de569a736
339713273 <339713273@2260I-L29-026>
2024-09-20 09:32:06 -0400

    Almost done

--- Program.cs
+++ Program.cs
@@ -4,19 +4,49 @@
     {
         static void Main(string[] args)
         {
-            // Carter Todd
-            string name;
-            int age;
-            double salery; 
-            Console.WriteLine("Please enter your name");
-            name = Console.ReadLine();
-            Console.WriteLine(name);
-            Console.WriteLine("What is your age?");
-            Int32.TryParse(Console.ReadLine(), out age);
-            Console.WriteLine("So you're " + age + ", eh? Thats not old at all!" );
-            Console.WriteLine("How much do you make, " + name + "?");
-            double.TryParse(Console.ReadLine(), out salery);
-            Console.WriteLine("13.50! I hope thats per hour and not per year!");
+            //// Carter Todd
+            //string name;
+            //int age;
+            //double salary;
+            //Console.WriteLine("Please enter your name");
+            //name = Console.ReadLine();
+            //Console.WriteLine(name);
+            //Console.WriteLine("What is your age?");
+            //Int32.TryParse(Console.ReadLine(), out age);
+            //Console.WriteLine("So you're " + age + ", eh? Thats not old at all!");
+            //Console.WriteLine("How much do you make, " + name + "?");
+            //double.TryParse(Console.ReadLine(), out salary);
+            //Console.WriteLine(salary.ToString("C")! + " I hope thats per hour and not per year!");
+
+
+            //string firstName, lastName, login;
+            //int ID, grade;
+            //double average;
+            //Console.WriteLine("hello what is your first name?"); 
+            //firstName = Console.ReadLine();
+            //Console.WriteLine("What is your last name?");
+            //lastName = Console.ReadLine();
+            //Console.WriteLine("What grade are you in?"); 
+            //grade = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("What is your student ID?");
+            //ID = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("What is your student login?");
+            //login = Console.ReadLine();
+            //Console.WriteLine("What is your average?");
+            //average = Convert.ToDouble(Console.ReadLine());
+
+
+            //string name;
+            //int age;
+            //Console.WriteLine("Hello. What is your name?");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Hi " + name);
+            //Console.WriteLine("How old are you?"); 
+            //Int32.TryParse(Console.ReadLine(), out age);
+            //Console.WriteLine("Did you know in 5 years you will be " + (age + 5) + " Years old?"); 
+            //Console.WriteLine("And 5 years ago you were " + (age - 5) + "! Imagine that!");
+            
+
 
         }
     }
